98dacb6c34c5ba949390e7329e7b44fc42a39565
Nicsap2005 <71231021@students.ukdw.ac.id>
2024-11-28 09:20:53 +0700

    Add files via upload

---
+++ project_so_linux/main.py
@@ -0,0 +1,82 @@
+from performances import Performances_stat
+from utility import util
+import tkinter as tk
+from tkinter import ttk
+
+class MainApp:
+    def __init__(self):
+        # Create the main window
+        self.window = tk.Tk()
+        self.performance_instance = Performances_stat()
+        self.utility_instance = util()
+        # Initialize the Label and Text widgets as attributes
+        self.performances_label = tk.Label(self.window, height=5, justify="left", text="Hello World")
+        self.text_box = tk.Text(self.window, height=1, width=38)
+        
+        ###########################################################################################
+        self.tree_box = tk.Text(self.window, height=20, width=70)#10,50
+        self.scrollbar = ttk.Scrollbar(self.window,orient="vertical", command=self.tree_box.yview)
+        ###########################################################################################
+        
+        self.graph_label = tk.Label(self.window, width=100, height=2)
+        # Set up the main window layout
+        self._setup_window()
+    
+    def _performances_btn_function(self,performance_instance, window, performances_label, graph_label,text_box,tree_box,scrollbar):
+        self._show_label()
+        self._else_btn_function(performances_label, graph_label,text_box,tree_box,scrollbar)
+        performance_instance.performances_btn(window, performances_label, graph_label)
+        
+    def _utility_btn_function(self,utility_instance, window, performances_label, graph_label,text_box,tree_box,scrollbar):
+        self._show_label()
+        self._else_btn_function(performances_label, graph_label, text_box,tree_box,scrollbar)
+        utility_instance.util_btn(window, performances_label, graph_label, text_box,tree_box,scrollbar)
+    
+    def _else_btn_function(self,performances_label, graph_label,text_box,tree_box,scrollbar):
+        self.performance_instance.elsebtn(performances_label, graph_label)
+        self.utility_instance.elsebtn(performances_label, graph_label,text_box,tree_box,scrollbar)
+                
+    def _setup_window(self):
+        # Place a left label
+        self.window.title("Project SO")
+        self.window.geometry("800x600")
+        
+        left_label = tk.Label(self.window, width=10, text="Project SO", font=("Arial", 12))
+        left_label.grid(row=0, column=0)
+        
+        # Initially display the Label and hide the Text widget
+        self.performances_label.grid(row=1, column=1, columnspan=8, rowspan=2)
+        
+        # Create other widgets
+        self.graph_label.grid(row=3, column=1, columnspan=100, rowspan=100)
+        
+        # Button to toggle to the Label view
+        performances_btn = tk.Button(self.window, width=12, height=2, text="Performances", borderwidth=2, relief="solid", 
+                                     command=lambda: self._performances_btn_function(self.performance_instance, self.window, self.performances_label, self.graph_label,self.text_box,self.tree_box,self.scrollbar))
+        performances_btn.grid(row=1, column=0)
+        
+        # Button to toggle to the Text view
+        utility_btn = tk.Button(self.window, width=12, height=2, text="Utility", borderwidth=2, relief="solid", 
+                                command=lambda:self._utility_btn_function(self.utility_instance, self.window, self.performances_label, self.graph_label,self.text_box,self.tree_box,self.scrollbar))
+        utility_btn.grid(row=2, column=0)
+        
+        # Another button
+        else_btn = tk.Button(self.window, width=12, height=2, text="Else", borderwidth=2, relief="solid", command=lambda:self._else_btn_function(self.performances_label, self.graph_label,self.text_box,self.tree_box,self.scrollbar))
+        else_btn.grid(row=3, column=0)
+
+        self.text_box.bind("<Return>", "break")
+        self.window.mainloop()
+
+    def _show_label(self):
+        # Hide the Text widget and show the Label
+        self.text_box.grid_forget()
+        self.performances_label.grid(row=1, column=1, columnspan=8, rowspan=2)
+    
+    def _show_text_box(self):
+        # Hide the Label and show the Text widget
+        self.performances_label.grid_forget()
+        self.text_box.grid(row=1, column=1, columnspan=8, rowspan=2)
+    
+
+if __name__ == "__main__":
+    app = MainApp()
---
+++ project_so_linux/utility.py
@@ -0,0 +1,150 @@
+from central_function import LinuxOS
+from ASCII_tree_documentation import tembelek
+import tkinter as tk
+
+class util:
+    def __init__(self):
+        self.ubuntu = LinuxOS()
+        
+        self.show_changedir_enabled = False
+        self.changedirButton = None
+        
+        self.show_getText_enabled = False
+        self.getTextButton = None
+        
+        self.show_tree_enabled = False
+        self.TreeButton = None
+        
+        self.Show_getTree_enabled = False
+        self.getTreeButton = None
+        
+        self.curpath = self.ubuntu.get_current_path()
+        self.avdir = "".join(self.ubuntu.get_available_directories())
+        
+    
+    def util_btn(self, window, performances_label, graph_label,text_box,tree_box,scrollbar):
+        self.changedir_btn(window, performances_label, graph_label,text_box,tree_box,scrollbar)
+        self.Tree_btn(window, performances_label, graph_label,text_box,tree_box,scrollbar)
+    
+    def clearing(self, performances_label, graph_label,text_box,tree_box,scrollbar):
+        self.remove_getTextButton_btn(text_box)
+        self.remove_getTreeButton_btn(text_box)
+        text_box.grid_forget()
+        tree_box.grid_forget()
+        scrollbar.grid_forget()
+        performances_label.grid_forget()
+        graph_label.grid_forget()
+        
+
+    ############### change directory ###############
+    
+    def update_changedir(self,window,performances_label, graph_label,text_box,tree_box,scrollbar):
+        self.clearing(performances_label, graph_label,text_box,tree_box,scrollbar)
+        performances_label.config(text="")
+        show_curpath = f"current path: {self.curpath}\navailable directory: {self.avdir}"
+        graph_label.config(height=2,width=62,text=show_curpath,justify="left")
+        graph_label.grid(row=1,column=1,columnspan=len(show_curpath),rowspan=10)
+
+        text_box.grid(row=1, column=0, columnspan=5, rowspan=1)
+        self.getText_btn(window,graph_label,text_box)
+
+    def changedir_btn(self, window, performances_label, graph_label,text_box,tree_box,scrollbar):
+        if self.changedirButton is None:  # Check if the button exists
+            self.changedirButton = tk.Button(window, width=15, height=1, text='change directory', borderwidth=2, relief='ridge', 
+                                        command=lambda:self.update_changedir(window,performances_label, graph_label,text_box,tree_box,scrollbar))
+            self.changedirButton.grid(row=0, column=1)
+        else:
+            self.changedirButton.grid(row=0, column=1)
+    
+    def getText_btn(self, window,graph_label,text_box):
+        if self.getTextButton is None:  # Check if the button exists
+            self.getTextButton = tk.Button(window, width=13, height=1, text='change directory', borderwidth=2,command=lambda:self.get_text(graph_label,text_box))
+            self.getTextButton.grid(row=1, column=3)
+        else:
+            self.getTextButton.grid(row=1, column=3)
+            
+    def get_text(self,graph_label,text_box):
+        content = text_box.get("1.0", "end-1c")
+        print("Text box content:", content)
+        self.ubuntu.change_directory(content)
+        self.curpath = self.ubuntu.get_current_path()
+        self.avdir = "".join(self.ubuntu.get_available_directories())
+
+        show_curpath = f"current path: {self.curpath}\navailable directory: {self.avdir}"
+        graph_label.config(text=show_curpath)
+        graph_label.grid(columnspan=len(show_curpath))
+
+    def remove_changedir_btn(self):
+        if self.changedirButton is not None:
+            self.changedirButton.grid_forget()  # Hide the button
+            self.changedirButton = None  # Reset the button reference
+            
+    def remove_getTextButton_btn(self,text_box):
+        text_box.grid_forget()
+        if self.getTextButton is not None:
+            self.getTextButton.grid_forget()  # Hide the button
+            self.getTextButton = None  # Reset the button reference
+        
+    ############### ASCII TREE ###############
+    
+    def update_Tree(self,window,performances_label, graph_label,text_box,tree_box,scrollbar):
+        self.clearing(performances_label, graph_label,text_box,tree_box,scrollbar)
+        
+        text_box.grid(row=1, column=0, columnspan=5, rowspan=1)
+        self.getTree_btn(window,graph_label,text_box,tree_box)
+        
+        tree_box.grid(row=3, column=1, rowspan=25,columnspan=15, sticky="nsew")
+        scrollbar.grid(row=3, column=20,rowspan=25,sticky="ns")
+    
+        tree_box.config(yscrollcommand=scrollbar.set)
+        
+
+    def Tree_btn(self, window, performances_label, graph_label,text_box,tree_box,scrollbar):
+        if self.TreeButton is None:  # Check if the button exists
+            self.TreeButton = tk.Button(window, width=15, height=1, text='ASCII Tree', borderwidth=2, relief='ridge', 
+                                        command=lambda:self.update_Tree(window,performances_label, graph_label,text_box,tree_box,scrollbar))
+            self.TreeButton.grid(row=0, column=2)
+        else:
+            self.TreeButton.grid(row=0, column=2)
+    
+    def getTree_btn(self, window,graph_label,text_box,tree_box):
+        if self.getTextButton is None:  # Check if the button exists
+            self.getTreeButton = tk.Button(window, width=13, height=1, text='Get Tree', borderwidth=2,command=lambda:self.get_tree(text_box,tree_box))
+            self.getTreeButton.grid(row=1, column=3)
+        else:
+            self.getTextButton.grid(row=1, column=3)
+            
+    def get_tree(self,text_box,tree_box):
+        tree_box.delete("1.0", tk.END)
+        content = text_box.get("1.0", "end-1c")
+        print("Text box content:", content)
+        tembelek(self.ubuntu,int(content), output_file_path='output.txt')
+        tree = ''
+        with open("output.txt") as output:
+            for i in output:
+                tree += i
+        tinggi = len(tree)
+        if tinggi > 50:
+            tinggi = len(tree)//20
+        
+        tree_box.insert(tk.END, tree + "\n")
+        
+    def remove_Tree_btn(self):
+        if self.TreeButton is not None:
+            self.TreeButton.grid_forget()  # Hide the button
+            self.TreeButton = None  # Reset the button reference
+        
+    def remove_getTreeButton_btn(self,text_box):
+        text_box.grid_forget()
+        if self.getTreeButton is not None:
+            self.getTreeButton.grid_forget()  # Hide the button
+            self.getTreeButton = None  # Reset the button reference
+    
+    ############### else button ###############
+    def elsebtn(self, performances_label, graph_label,text_box,tree_box,scrollbar):
+        self.show_available_directory_enabled = False
+        self.clearing(performances_label, graph_label,text_box,tree_box,scrollbar)
+        self.remove_changedir_btn()
+        self.remove_getTextButton_btn(text_box)
+        self.remove_Tree_btn()
+        self.remove_getTreeButton_btn(text_box)
